2089c6be87d012e3ace3b6dbcc8031f2f2db3b58
John Tolentino <jpftolentino@gmail.com>
2017-08-01 18:14:27 +0000

    added gem faker and finished Exercise 6

--- Gemfile
+++ Gemfile
@@ -5,3 +5,4 @@ gem 'pry' # for debugging
 gem 'pg'
 gem 'activerecord'
 gem 'postgresql'
+gem 'faker'
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,24 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(
+    first_name: "Khurram", 
+    last_name: "Virani", 
+    hourly_rate: 60
+)
+
+1.upto(10) do
+    @store1.employees.create(
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        hourly_rate: Faker::Number.between(40,60)
+    )
+end
+
+1.upto(10) do
+    @store2.employees.create(
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        hourly_rate: Faker::Number.between(40,200)
+    )
+end
